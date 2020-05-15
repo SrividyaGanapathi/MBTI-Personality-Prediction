e2e16b4db6f47fdb14bef021dbeee1ba8a263ba2
SrividyaGanapathi <55149874+SrividyaGanapathi@users.noreply.github.com>
2020-05-15 13:36:55 -0500

    Update boot.sh

--- app/boot.sh
+++ app/boot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python3 run1.py load_data --where=Upload
-#python3 run.py create_db
-#python3 run.py ingest
-#python3 app.py
+
+python3 run.py create_db
+python3 run.py ingest
+python3 app.py
